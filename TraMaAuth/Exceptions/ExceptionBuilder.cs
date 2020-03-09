bba89ae72cb375b1202b9c07ddad9a0e9594b888
Marco Dallera <marco.dallera@libero.it>
2020-03-09 15:39:59 +0100

    Aggiunte classi per gestione eccezioni su database

---
+++ TraMaAuth/Exceptions/ExceptionBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace Cizeta.TraMaAuth
+{
+    static class ExceptionBuilder
+    {
+        internal static string ComposeMessage(string className, string methodName, Exception ex)
+        {
+            return string.Format("[{0}.{1}] {2}", className, methodName, ex.Message);
+        }
+
+        internal static string ComposeMessage(string className, string methodName, Exception ex, params string[] parameters)
+        {
+            string parametersString = string.Empty;
+            for (int i = 0; i < parameters.Count(); i++)
+            {
+                if (i == 0)
+                    parametersString += string.Format("{0}", parameters[i]);
+                else
+                    parametersString += string.Format(", {0}", parameters[i]);
+            }
+            return string.Format("[{0}.{1}({2})] {3}", className, methodName, parametersString, ex.Message);
+        }
+    }
+}
