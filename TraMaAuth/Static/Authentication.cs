932de23ebed84e3623aa5bf9ec627b95a1754a10
Marco Dallera <marco.dallera@libero.it>
2020-09-30 17:51:02 +0200

    Aggiunto classe statica Authentication per inizializzazioni e ConnectionString

---
+++ TraMaAuth/Static/Authentication.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Reflection;
+
+namespace Cizeta.TraMaAuth
+{
+    public static class Authentication
+    {
+        public static void Init(string connectionString)
+        {
+            InitConnectionString(connectionString);
+            InitAuthenticationModes();
+            InitWorkerFunctions();
+        }
+
+        private static void InitConnectionString(string connectionString)
+        {
+            Properties.Settings.Default["TraMaConnectionString"] = connectionString;
+        }
+
+        private static void InitAuthenticationModes()
+        {
+            try
+            {
+                int id = 0;
+                string name = string.Empty;
+                string description = string.Empty;
+                foreach (AuthenticationMode am in Enum.GetValues(typeof(AuthenticationMode)))
+                {
+                    try
+                    {
+                        id = (int)am;
+                        name = am.ToString();
+                        description = name;
+                        using (WorkersDataSetTableAdapters.QueriesTableAdapter q = new WorkersDataSetTableAdapters.QueriesTableAdapter())
+                        {
+                            q.MergeAuthenticationMode(id, name, description);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ExceptionBuilder.ComposeMessage(
+                            MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, ex,
+                            new string[] { id.ToString(), name, description }));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ExceptionBuilder.ComposeMessage(
+                    MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, ex));
+            }
+        }
+
+        private static void InitWorkerFunctions()
+        {
+            try
+            {
+                int id = 0;
+                string name = string.Empty;
+                string description = string.Empty;
+                foreach (WorkerFunction value in Enum.GetValues(typeof(WorkerFunction)))
+                {
+                    try
+                    {
+                        id = (int)value;
+                        name = value.ToString();
+                        description = name;
+                        using (WorkersDataSetTableAdapters.QueriesTableAdapter q = new WorkersDataSetTableAdapters.QueriesTableAdapter())
+                        {
+                            q.MergeWorkerFunction(id, name, description);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception(ExceptionBuilder.ComposeMessage(
+                            MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, ex,
+                            new string[] { id.ToString(), name, description }));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ExceptionBuilder.ComposeMessage(
+                    MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, ex));
+            }
+        }
+    }
+}
